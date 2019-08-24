d33de2d52b95aa35f80043e9161e2cb2a5cc2c58
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-24 01:29:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,26 @@
 class School 
   
-  ROSTER = {}
-  
   def initialize(name)
-    @name = name 
+    @name = name
+    @roster = {}
   end
+  
+  def add_student(student_name, student_grade)
+    roster[student_grade] = []
+    roster[student_grade] << student_name
+  end 
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  
+  def sort 
+    student_sort = {}
+    roster.each do |student_grade, students|
+      student_sort[student_grade] = students.sort 
+    end
+    
+    return student_sort
+  end 
+end
+    
