d16883d2b9d6e73266d3547961968658d3d878bb
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-24 01:41:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,7 @@ class School
   
   def add_student(student_name, student_grade)
     roster[student_grade] = []
+    return roster[student_grade]
     roster[student_grade] << student_name
   end 
   
